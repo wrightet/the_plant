63babf87ad802d2260d2ca827d5ff50c73f10196
Ethan Wright <wrightet707@gmail.com>
2021-03-11 20:36:33 -0800

    test soil and pump and launch version 1.0

--- garden.ino
+++ garden.ino
@@ -2,32 +2,32 @@ int soilPin = A0;
 int soilVal;
 int waterLevelPin = A1;
 int waterVal;
-int dt = 10000;
 int pump = 8;
-// the pump should be off for at least 3 days to let soil dry;
-int timeoff = 259200000; 
+int timeoff = 1000;
 // the hose and the pump put out about 2 tsps in 1 sec which is enough for the cactus
-int timeon = 1000; 
+int timeon = 1000;
 void setup()
 {
     Serial.begin(9600);
     pinMode(soilPin, INPUT);
     pinMode(waterLevelPin, INPUT);
-    // pinMode(pump, OUTPUT);
-    // digitalWrite(pump, HIGH);
+    pinMode(pump, OUTPUT);
+    digitalWrite(pump, HIGH);
 }
 
 void loop()
 {
-    // digitalWrite(pump, LOW);
-    // dt(timeon);
-    // digitalwrite(pump, HIGH);
-    // dt(timeoff);
-    waterVal = analogRead(waterLevelPin);
+    if (soilVal > 850)
+    {
+        digitalWrite(pump, LOW);
+        delay(timeon);
+        digitalWrite(pump, HIGH);
+    }
+    //    waterVal = analogRead(waterLevelPin);
     soilVal = analogRead(soilPin);
-    Serial.print("Water Level: ");
-    Serial.print(waterVal);
+    //    Serial.print("Water Level: ");
+    //    Serial.print(waterVal);
     Serial.print(" Soil Value: ");
     Serial.println(soilVal);
-    delay(dt);
+    delay(timeoff);
 }
